446d6d4a41c294f5d07069ce230bd57b07f2d9a7
semen <semen.dorohov@yandex.ru>
2023-02-17 16:50:00 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿//// Найти сумму элементов от M до N, N и M заданы
+
+Console.WriteLine("Введите начальное и конечное значения числового ряда");
+int M = int.Parse(Console.ReadLine() ?? "");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+int SumNumbers(int M, int N)
+{
+    if (M > N)
+    {
+        return 0;
+    }
+    else
+    {
+        return M + SumNumbers(M + 1, N);
+    }
+}
+Console.WriteLine($"Сумма чисел от {M} до {N} равна {SumNumbers(M, N)}");

4ebf61dbb4d4b00aaaccab60ac2c380cd3aff4d4
semen <semen.dorohov@yandex.ru>
2023-02-17 16:47:41 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//// Показать натуральные числа от M до N, N и M заданы
+
+Console.WriteLine("Введите начальное и конечное значения числового ряда");
+int M = int.Parse(Console.ReadLine() ?? "");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+string NumbersLength(int M, int N)
+{
+    if (M <= N)
+    {
+        return $"{M} " + NumbersLength(M+1, N);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
+Console.WriteLine(NumbersLength(M, N));

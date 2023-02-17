4d119a4a40e3e854dcb82a66e7bb9f0957e65a8b
semen <semen.dorohov@yandex.ru>
2023-02-17 16:52:00 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿//// Найти сумму цифр числа
+
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine() ?? "");
+int SumNumbers(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return num % 10 + SumNumbers(num / 10);
+    }
+}
+Console.WriteLine(SumNumbers(num));

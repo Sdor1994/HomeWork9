4163b716bd909ef2481f85b680e50d021715f717
semen <semen.dorohov@yandex.ru>
2023-02-17 17:32:52 +0300

    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,20 @@
+﻿/// Написать программу возведения числа А в целую стень B
+
+
+Console.WriteLine("Введите число А ");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите степень В ");
+int b = int.Parse(Console.ReadLine() ?? "");
+
+int Result(int a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return Result(a, b - 1) * a;
+    }
+}
+Console.WriteLine($"Число {a} в степени {b} = {Result(a, b)}");

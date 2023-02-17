c062388434a80cbc45606e888e46dd853195c1ad
semen <semen.dorohov@yandex.ru>
2023-02-17 16:43:18 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//// Показать натуральные числа от N до 1, N задано
+
+Console.WriteLine("Введите первое натуральное число в ряду: ");
+int Num = 1;
+int Num2 = int.Parse(Console.ReadLine() ?? "");
+
+string NumbersLength(int Num2, int Num)
+{
+    if (Num2 >= Num)
+    {
+        return $"{Num2} " + NumbersLength(Num2-1, Num);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
+Console.WriteLine(NumbersLength(Num2, Num));
